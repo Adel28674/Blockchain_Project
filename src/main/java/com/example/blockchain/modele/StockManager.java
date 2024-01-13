3027640b0bb3e83698137ef6891b419dc2f4eb5d
Adel <adelhammiche.pro@gmail.com>
2024-01-13 14:14:44 +0100

    Ajout de la dépendance Alpha Vantage et de la clé api

--- src/main/java/com/example/blockchain/modele/StockManager.java
+++ src/main/java/com/example/blockchain/modele/StockManager.java
@@ -1,27 +1,44 @@
 package com.example.blockchain.modele;
 
-import yahoofinance.Stock;
-import yahoofinance.YahooFinance;
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.AlphaVantageException;
+import com.crazzyghost.alphavantage.Config;
+import com.crazzyghost.alphavantage.timeseries.TimeSeries;
+import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
+
+
 
 import java.io.IOException;
+import java.io.ObjectInputFilter;
 import java.math.BigDecimal;
+
+import static com.crazzyghost.alphavantage.AlphaVantage.*;
 
 public class StockManager {
     public static void main(String[] args) throws IOException {
-        Stock stock = null;
-        try{
-            stock = YahooFinance.get("TSLA");
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+
+        // Remplacez 'YOUR_API_KEY' par votre propre clé API Alpha Vantage
+        String apiKey = "RUCNTNS1KKTTBZUV";
+
+        // Configurez Alpha Vantage avec votre clé API
+        Config config = Config.builder().key(apiKey).build();
+        api().init(config);
+
+        try {
+            // Récupérez les données de série temporelle pour les actions de Google
+
+
+            TimeSeriesResponse timeSeriesResponse = AlphaVantage.api()
+                    .timeSeries()
+                    .intraday()
+                    .fetchSync();
+
+            System.out.println(timeSeriesResponse.toString());
+
+
+        } catch (AlphaVantageException e) {
+            e.printStackTrace();
         }
-
-        BigDecimal price = stock.getQuote().getPrice();
-        BigDecimal change = stock.getQuote().getChangeInPercent();
-        BigDecimal peg = stock.getStats().getPeg();
-        BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-
-
-        stock.print();
     }
 
 }
